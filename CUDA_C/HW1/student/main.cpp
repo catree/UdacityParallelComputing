00aa7fd5c2721ab322ae9c8fe403dc33b8dbbe66
Erich Elsen <erich@royal-caliber.com>
2013-01-07 11:04:19 -0800

    HW1.cpp is now included directly in main and not compiled separately

--- CUDA_C/HW1/student/main.cpp
+++ CUDA_C/HW1/student/main.cpp
@@ -4,24 +4,22 @@
 #include "timer.h"
 #include "utils.h"
 #include <string>
+#include <stdio.h>
 
 size_t numRows();  //return # of rows in the image
 size_t numCols();  //return # of cols in the image
 
-//load image into both host and device memory space
-//allocate memory for result in both memory spaces
-//input = rgbaImage; output = greyImage
-//referenceCalculation fills in h_greyImage
-//you need to fill in d_greyImage
 void preProcess(uchar4 **h_rgbaImage, unsigned char **h_greyImage,
                 uchar4 **d_rgbaImage, unsigned char **d_greyImage,
                 const std::string& filename);
 
-//check the results and write out the final image
 void postProcess(const std::string& output_file);
 
 void your_rgba_to_greyscale(const uchar4 * const h_rgbaImage, uchar4 * const d_rgbaImage,
                             unsigned char* const d_greyImage, size_t numRows, size_t numCols);
+
+//include the definitions of the above functions for this homework
+#include "HW1.cpp"
 
 int main(int argc, char **argv) {
   uchar4        *h_rgbaImage, *d_rgbaImage;
@@ -44,7 +42,13 @@ int main(int argc, char **argv) {
   //call the students' code
   your_rgba_to_greyscale(h_rgbaImage, d_rgbaImage, d_greyImage, numRows(), numCols());
   checkCudaErrors(cudaDeviceSynchronize());
-  std::cout << "e57__TIMING__f82 Processing took: " << tick() << " msecs." << std::endl;
+  int err = printf("e57__TIMING__f82 Processing took: %f msecs.\n", tick());
+  
+  if (err < 0) {
+    //Couldn't print! Probably the student closed stdout - bad news
+    std::cerr << "Couldn't print timing information! STDOUT Closed!" << std::endl;
+    exit(1);
+  }
 
   //check results and output the grey image
   postProcess(output_file);
