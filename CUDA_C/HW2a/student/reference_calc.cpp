ef9446f8dbf62bcf964b3cc8ff1374fdc5c85d58
Erich Elsen <erich@royal-caliber.com>
2013-01-10 13:14:51 -0800

    added more comments to reference calculation

--- CUDA_C/HW2a/student/reference_calc.cpp
+++ CUDA_C/HW2a/student/reference_calc.cpp
@@ -6,13 +6,18 @@ void channelConvolution(const unsigned char* const channel,
                         const size_t numRows, const size_t numCols,
                         const float *filter, const int filterWidth)
 {
+  //Dealing with an even width filter is trickier
   assert(filterWidth % 2 == 1);
 
+  //For every pixel in the image
   for (int r = 0; r < (int)numRows; ++r) {
     for (int c = 0; c < (int)numCols; ++c) {
       float result = 0.f;
+      //For every value in the filter around the pixel (c, r)
       for (int filter_r = -filterWidth/2; filter_r <= filterWidth/2; ++filter_r) {
         for (int filter_c = -filterWidth/2; filter_c <= filterWidth/2; ++filter_c) {
+          //Find the global image position for this filter position
+          //clamp to boundary of the image
           int image_r = min(max(r + filter_r, 0), static_cast<int>(numRows - 1));
           int image_c = min(max(c + filter_c, 0), static_cast<int>(numCols - 1));
 
@@ -32,8 +37,6 @@ void referenceCalculation(const uchar4* const rgbaImage, uchar4 *const outputIma
                           size_t numRows, size_t numCols,
                           const float* const filter, const int filterWidth)
 {
-  //first order of business is to separate the channels
-
   unsigned char *red   = new unsigned char[numRows * numCols];
   unsigned char *blue  = new unsigned char[numRows * numCols];
   unsigned char *green = new unsigned char[numRows * numCols];
@@ -42,6 +45,8 @@ void referenceCalculation(const uchar4* const rgbaImage, uchar4 *const outputIma
   unsigned char *blueBlurred  = new unsigned char[numRows * numCols];
   unsigned char *greenBlurred = new unsigned char[numRows * numCols];
 
+  //First we separate the incoming RGBA image into three separate channels
+  //for Red, Green and Blue
   for (size_t i = 0; i < numRows * numCols; ++i) {
     uchar4 rgba = rgbaImage[i];
     red[i]   = rgba.x;
@@ -54,8 +59,7 @@ void referenceCalculation(const uchar4* const rgbaImage, uchar4 *const outputIma
   channelConvolution(green, greenBlurred, numRows, numCols, filter, filterWidth);
   channelConvolution(blue, blueBlurred, numRows, numCols, filter, filterWidth);
 
-  //now recombine into the output image
-
+  //now recombine into the output image - Alpha is 255 for no transparency
   for (size_t i = 0; i < numRows * numCols; ++i) {
     uchar4 rgba = make_uchar4(redBlurred[i], greenBlurred[i], blueBlurred[i], 255);
     outputImage[i] = rgba;
