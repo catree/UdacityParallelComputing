0450d10b4cc4f825b34ad67c0cc4b37752d95517
Erich Elsen <erich@royal-caliber.com>
2013-01-11 15:16:28 -0800

    checkCudaErrors was missing kernel launch errors, caught now

--- CUDA_C/HW3/student/utils.h
+++ CUDA_C/HW3/student/utils.h
@@ -5,13 +5,15 @@
 #include <iomanip>
 #include <cuda.h>
 #include <cuda_runtime.h>
+#include <cuda_runtime_api.h>
 #include <cassert>
 #include <cmath>
 
-#define checkCudaErrors(val) check( (val), #val, __FILE__, __LINE__)
+#define checkCudaErrors(val) check( #val, __FILE__, __LINE__)
 
 template<typename T>
-void check(T err, const char* const func, const char* const file, const int line) {
+void check(T func, const char* const file, const int line) {
+  cudaError_t err = cudaGetLastError();
   if (err != cudaSuccess) {
     std::cerr << "CUDA error at: " << file << ":" << line << std::endl;
     std::cerr << cudaGetErrorString(err) << " " << func << std::endl;
