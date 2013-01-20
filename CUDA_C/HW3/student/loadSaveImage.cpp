78618f8756f6ccc50377c9f8a4d3141562a89490
Erich Elsen <erich@royal-caliber.com>
2013-01-19 21:37:55 -0800

    making loadSaveImage versions match

--- CUDA_C/HW3/student/loadSaveImage.cpp
+++ CUDA_C/HW3/student/loadSaveImage.cpp
@@ -39,6 +39,43 @@ void loadImageHDR(const std::string &filename,
   *numCols = image.cols;
 }
 
+void loadImageRGBA(const std::string &filename,
+                   uchar4 **imagePtr,
+                   size_t *numRows, size_t *numCols)
+{
+  cv::Mat image = cv::imread(filename.c_str(), CV_LOAD_IMAGE_COLOR);
+  if (image.empty()) {
+    std::cerr << "Couldn't open file: " << filename << std::endl;
+    exit(1);
+  }
+
+  if (image.channels() != 3) {
+    std::cerr << "Image must be color!" << std::endl;
+    exit(1);
+  }
+
+  if (!image.isContinuous()) {
+    std::cerr << "Image isn't continuous!" << std::endl;
+    exit(1);
+  }
+
+  cv::Mat imageRGBA;
+  cv::cvtColor(image, imageRGBA, CV_BGR2RGBA);
+
+  *imagePtr = new uchar4[image.rows * image.cols];
+
+  unsigned char *cvPtr = imageRGBA.ptr<unsigned char>(0);
+  for (size_t i = 0; i < image.rows * image.cols; ++i) {
+    (*imagePtr)[i].x = cvPtr[4 * i + 0];
+    (*imagePtr)[i].y = cvPtr[4 * i + 1];
+    (*imagePtr)[i].z = cvPtr[4 * i + 2];
+    (*imagePtr)[i].w = cvPtr[4 * i + 3];
+  }
+
+  *numRows = image.rows;
+  *numCols = image.cols;
+}
+
 void saveImageRGBA(const uchar4* const image,
                    const size_t numRows, const size_t numCols,
                    const std::string &output_file)
--- CUDA_C/HW3/student/loadSaveImage.h
+++ CUDA_C/HW3/student/loadSaveImage.h
@@ -8,6 +8,10 @@ void loadImageHDR(const std::string &filename,
                   float **imagePtr,
                   size_t *numRows, size_t *numCols);
 
+void loadImageRGBA(const std::string &filename,
+                   uchar4 **imagePtr,
+                   size_t *numRows, size_t *numCols);
+
 void saveImageRGBA(const uchar4* const image,
                    const size_t numRows, const size_t numCols,
                    const std::string &output_file);
