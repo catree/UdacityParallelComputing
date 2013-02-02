a55b65421374a454dbfe1e3b649eb5f2fe8e5f7d
Erich Elsen <erich@royal-caliber.com>
2013-02-01 17:21:09 -0800

    modified text to be more correct regarding the color mapping actuallying being done and Chrominance-Luminance.  They are the same idea, but slightly different actual mappings. Also formatted text to be 80 columns wide

--- CUDA_C/HW3/student/student_func.cu
+++ CUDA_C/HW3/student/student_func.cu
@@ -8,53 +8,59 @@
   and color than is allowed by the RGB format with 1 byte per channel that we
   have used in the previous assignment.  
 
-  To store this extra information we use single precision floating point
-  for each channel.  This allows for an extremely wide range of intensity values.
+  To store this extra information we use single precision floating point for
+  each channel.  This allows for an extremely wide range of intensity values.
 
-  In the image for this assignment, the inside of church with light coming in through
-  stained glass windows, the raw input floating point values for the
-  channels range from 0 to 275.  But the mean is .41 and 98% of the values are less than 3!  This
-  means that certain areas (the windows) are extremely bright compared to everywhere else.
-  If we linearly map this [0-275] range into the [0-255] range that we have been using
-  then most values will be mapped to zero! 
+  In the image for this assignment, the inside of church with light coming in
+  through stained glass windows, the raw input floating point values for the
+  channels range from 0 to 275.  But the mean is .41 and 98% of the values are
+  less than 3!  This means that certain areas (the windows) are extremely bright
+  compared to everywhere else.  If we linearly map this [0-275] range into the
+  [0-255] range that we have been using then most values will be mapped to zero!
   The only thing we will be able to see are the very brightest areas - the
   windows - everything else will appear pitch black.
 
-  The problem is that although we have cameras capable of recording the wide range
-  of intensity that exists in the real world our monitors are not capable of displaying them.
-  Our eyes are also quite capable of observing a much wider range of intensities than
-  our image formats / monitors are capable of displaying.
+  The problem is that although we have cameras capable of recording the wide
+  range of intensity that exists in the real world our monitors are not capable
+  of displaying them.  Our eyes are also quite capable of observing a much wider
+  range of intensities than our image formats / monitors are capable of
+  displaying.
 
-  Tone-mapping is a process that transforms the intensities in the image so that the
-  brightest values aren't nearly so far away from the mean.  That way when we transform
-  the values into [0-255] we can actually see the entire image.  There are many ways
-  to perform this process and it is as much an art as a science - there is no single
-  "right" answer.  In this homework we will implement one possible technique.
+  Tone-mapping is a process that transforms the intensities in the image so that
+  the brightest values aren't nearly so far away from the mean.  That way when
+  we transform the values into [0-255] we can actually see the entire image.
+  There are many ways to perform this process and it is as much an art as a
+  science - there is no single "right" answer.  In this homework we will
+  implement one possible technique.
 
   Background Chrominance-Luminance
   ================================
 
-  The RGB space that we have been using to represent images can be thought of
-  as one possible set of axes spanning a three dimensional space of color.
-  We sometimes choose other axes to represent this space because
-  they make certain operations more convenient.
+  The RGB space that we have been using to represent images can be thought of as
+  one possible set of axes spanning a three dimensional space of color.  We
+  sometimes choose other axes to represent this space because they make certain
+  operations more convenient.
 
-  Another possible set of axes is known as Chrominance-Luminance or xyY.
-  This set of axes separates the color and intensity information completely -
-  that is, the Luminance channel is actually a greyscale version of the image
-  and the two Chrominance channels carry the color information.
+  Another possible way of representing a color image is to separate the color
+  information (chromaticity) from the brightness information.  There are
+  multiple different methods for doing this - a common one during the analog
+  television days was known as Chrominance-Luminance or YUV.
+
+  We choose to represent the image in this way so that we can remap only the
+  intensity channel and then recombine the new intensity values with the color
+  information to form the final image.
 
   Old TV signals used to be transmitted in this way so that black & white
-  televisions could display the luminance channel while color televisions
-  would display all three of the channels.
+  televisions could display the luminance channel while color televisions would
+  display all three of the channels.
   
 
   Tone-mapping
   ============
 
-  In this assignment we are going to transform the luminance channel
-  (actually the log of the luminance, but this is unimportant for the parts of the
-   algorithm that you will be implementing) by compressing its range to [0, 1].
+  In this assignment we are going to transform the luminance channel (actually
+  the log of the luminance, but this is unimportant for the parts of the
+  algorithm that you will be implementing) by compressing its range to [0, 1].
   To do this we need the cumulative distribution of the luminance values.
 
   Example
@@ -68,7 +74,8 @@
   cdf : [4 11 14]
 
 
-  Your task is to calculate this cumulative distribution by following these steps.
+  Your task is to calculate this cumulative distribution by following these
+  steps.
 
 */
 
